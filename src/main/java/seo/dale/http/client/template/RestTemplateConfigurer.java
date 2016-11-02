590145b1d2e13d9b13b5efcdf25590dfd9b9c674
Dale Seo <dale.seo@gmail.com>
2016-11-02 23:01:16 +0900

    RestTemplateConfigurer

---
+++ src/main/java/seo/dale/http/client/template/RestTemplateConfigurer.java
@@ -0,0 +1,56 @@
+package seo.dale.http.client.template;
+
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriTemplateHandler;
+import seo.dale.http.client.intercept.HeaderSupplementInterceptor;
+import seo.dale.http.client.resolve.BaseUrlResolver;
+import seo.dale.http.client.resolve.HeaderResolver;
+import seo.dale.http.client.url.BaseUrlUriTemplateHandler;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class RestTemplateConfigurer {
+
+	private RestTemplate restTemplate;
+
+	public RestTemplateConfigurer(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
+
+	/**
+	 * baseUrlProvider로 BaseUrlUriTemplateHandler를 만들어 restTemplate에 설정해준다.
+	 */
+	public void configure(BaseUrlResolver baseUrlProvider) {
+		if (baseUrlProvider == null) {
+			return;
+		}
+		UriTemplateHandler uriTemplateHandler = new BaseUrlUriTemplateHandler(baseUrlProvider);
+		restTemplate.setUriTemplateHandler(uriTemplateHandler);
+	}
+
+	/**
+	 * headerResolverr로 HeaderSupplementInterceptor을 만들어 restTemplate에 설정해준다.
+	 */
+	public void configure(HeaderResolver headerResolver) {
+		if (headerResolver == null) {
+			return;
+		}
+
+		List<ClientHttpRequestInterceptor> interceptors = restTemplate.getInterceptors();
+		HeaderSupplementInterceptor headerSupplementInterceptor = new HeaderSupplementInterceptor(headerResolver);
+
+		if (interceptors == null) {
+			interceptors = Collections.singletonList(headerSupplementInterceptor); // 단독 인터셉터
+		} else {
+			interceptors = new ArrayList<>(interceptors); // in case of an immutable list
+			interceptors.add(0, headerSupplementInterceptor); // 맨 앞 인터셉터
+		}
+
+		restTemplate.setInterceptors(interceptors);
+	}
+
+
+}
---
+++ src/test/java/seo/dale/http/client/template/RestTemplateConfigurerTest.java
@@ -0,0 +1,59 @@
+package seo.dale.http.client.template;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestTemplate;
+import seo.dale.http.client.intercept.HeaderSupplementInterceptor;
+import seo.dale.http.client.resolve.BaseUrlResolver;
+import seo.dale.http.client.resolve.HeaderResolver;
+import seo.dale.http.client.url.BaseUrlUriTemplateHandler;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RestTemplateConfigurerTest {
+
+	private RestTemplate restTemplate;
+	private RestTemplateConfigurer configurer;
+
+	@Before
+	public void setUp() {
+		restTemplate = new RestTemplate();
+		configurer = new RestTemplateConfigurer(restTemplate);
+	}
+
+	@Test
+	public void testConfigureBaseUrlResolver() {
+		BaseUrlResolver baseUrlResolver = () -> "https://jsonplaceholder.typicode.com";
+		configurer.configure(baseUrlResolver);
+
+		assertThat(restTemplate.getUriTemplateHandler()).isInstanceOf(BaseUrlUriTemplateHandler.class);
+	}
+
+	@Test
+	public void testConfigureHeaderResolver() {
+		HeaderResolver headerResolver = new HeaderResolver() {
+			@Override
+			public String resolveAuthKey() {
+				return "85f1f03";
+			}
+
+			@Override
+			public MediaType resolveContentType() {
+				return MediaType.APPLICATION_JSON_UTF8;
+			}
+
+			@Override
+			public List<MediaType> resolveAccept() {
+				return Collections.singletonList(MediaType.APPLICATION_JSON_UTF8);
+			}
+		};
+		configurer.configure(headerResolver);
+
+		assertThat(restTemplate.getInterceptors().get(0)).isInstanceOf(HeaderSupplementInterceptor.class);
+	}
+
+}
