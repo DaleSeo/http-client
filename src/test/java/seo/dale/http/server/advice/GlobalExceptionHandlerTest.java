0427cd29eb4b2c167f49de96ffcd366850e49cea
Dale Seo <dale.seo@gmail.com>
2017-02-20 13:42:44 +0900

    init react

--- src/test/java/seo/dale/http/server/advice/GlobalExceptionHandlerTest.java
+++ src/test/java/seo/dale/http/server/advice/GlobalExceptionHandlerTest.java
@@ -2,18 +2,14 @@ package seo.dale.http.server.advice;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.stereotype.Controller;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.filter.OncePerRequestFilter;
-import seo.dale.http.web.test.ErrorTestController;
-
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class GlobalExceptionHandlerTest {
@@ -23,13 +19,7 @@ public class GlobalExceptionHandlerTest {
     @Before
     public void setUp() {
 
-        mvc = MockMvcBuilders.standaloneSetup(new ErrorTestController())
-		        .addFilter(new OncePerRequestFilter() {
-			        @Override
-			        protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-				        throw new RuntimeException("Filter Exception");
-			        }
-		        })
+        mvc = MockMvcBuilders.standaloneSetup(new MockController())
                 .setControllerAdvice(new GlobalExceptionHandler())
                 .build();
     }
@@ -37,10 +27,19 @@ public class GlobalExceptionHandlerTest {
     @Test
     public void testGet() throws Exception {
         mvc.perform(
-                get("/error/runtime")
+                get("/error")
         )
                 .andDo(print())
-                .andExpect(status().isInternalServerError());
+                .andExpect(status().isInternalServerError())
+                .andExpect(content().string("java.lang.RuntimeException: TEST ERROR"));
+    }
+
+    @Controller
+    static class MockController {
+        @RequestMapping("/error")
+        public void error() {
+            throw new RuntimeException("TEST ERROR");
+        }
     }
 
 }
